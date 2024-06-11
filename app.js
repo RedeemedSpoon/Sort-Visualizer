80557f9a7ad76144ab26e6e4192310e43bcd77cc
Ethan <QustoTemmo@protonmail.com>
2024-06-11 19:28:44 +0100

    Building The Basis 2

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 import express from 'express';
-import {allSorts, getExample, getInfo} from './sort.js';
+import * as fs from 'node:fs/promises';
 
 const app = express();
 const year = new Date().getFullYear();
+
+const comparisonSort = ['heap', 'merge', 'tim', 'quick', 'shell', 'bubble', 'insertion', 'cocktail', 'gnome', 'pancake', 'selection'];
+const nonComparisonSort = ['pigeonhole', 'radix', 'counting', 'bucket', 'flash'];
+const otherSort = ['bead', 'spaghetti', 'bitonic', 'bogo', 'stooge'];
+const allSorts = [...comparisonSort, ...nonComparisonSort, ...otherSort];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home.ejs', {year, title: 'Home'});
+  const title = 'Home';
+  res.render('home.ejs', {year, title});
 });
 
 allSorts.forEach((sortAlgo) => {
-  app.get(`/${sortAlgo}`, (req, res) => {
-    const data = getInfo(sortAlgo);
-    const title = `${titleCase(sortAlgo)} Sort`;
+  app.get(`/${sortAlgo}`, async (req, res) => {
+    const data = await getInfo(sortAlgo);
+    const title = data['Name'];
     res.render('playground.ejs', {year, title, data});
   });
 });
 
-app.get('/custom', (req, res) => {
-  const example = getExample()
-  res.render('custom.ejs', {year, title: 'Custom Sort', example});
+app.get('/custom', async (req, res) => {
+  const example = await getInfo('custom');
+  const title = 'Custom Sort';
+  res.render('custom.ejs', {year, title, example});
 });
 
-const titleCase = (word) => {
-  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+const getInfo = async (algo) => {
+  const file = `algorithms/${algo}.json`;
+  const data = await fs.readFile(file, 'utf8');
+  return JSON.parse(data);
 };
 
 const handleError = (req, res, next) => {
-  return res.status(404).render('error.ejs', {year, title: 'Error 404'});
+  const title = 'Error 404';
+  return res.status(404).render('error.ejs', {year, title});
 };
 
 app.use(handleError);
