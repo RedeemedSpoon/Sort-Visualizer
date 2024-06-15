9fc061d239a271d2e151fbfe8a1f9d1c14306562
Ethan <QustoTemmo@protonmail.com>
2024-06-16 00:13:36 +0100

    Nav Section Bar & Error Page

--- public/script.js
+++ public/script.js
@@ -3,15 +3,41 @@ const themeToggleBtn = document.getElementById('toggle');
 const lightThemeImg = document.getElementById('light');
 const darkThemeImg = document.getElementById('dark');
 
+const selectorBtn = document.getElementById('algo-selector');
+const overlay = document.getElementById('overlay');
+const navBar = document.querySelector('nav');
+
+// NavBar
+selectorBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleNavBar();
+});
+
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('nav')) {
+    navBar.classList.contains('slide-in') && toggleNavBar();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+    toggleNavBar();
+  }
+});
+
+const toggleNavBar = () => {
+  const isOpen = navBar.classList.contains('slide-in');
+  navBar.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
+  overlay.classList.toggle('active');
+};
+
+// Theme
 document.addEventListener('DOMContentLoaded', function () {
   const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
   const storedTheme = localStorage.getItem('color-theme');
   const theme = storedTheme || (prefersDarkMode.matches ? 'dark' : 'light');
-
-  if (theme === 'dark') {
-    document.body.classList.add('dark');
-    addDarkTheme();
-  }
+  theme === 'dark' && addDarkTheme();
 });
 
 [themeToggle, themeToggleBtn].forEach((element) => {
@@ -28,6 +54,7 @@ const addDarkTheme = () => {
   darkThemeImg.classList.add('active');
   themeToggle.classList.add('night');
   themeToggleBtn.classList.add('left');
+  navBar.id = 'dark-mode';
 };
 
 const removeDarkTheme = () => {
@@ -36,4 +63,5 @@ const removeDarkTheme = () => {
   darkThemeImg.classList.remove('active');
   themeToggle.classList.remove('night');
   themeToggleBtn.classList.remove('left');
+  navBar.removeAttribute('id');
 };
